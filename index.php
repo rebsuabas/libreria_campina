05ed5d74a51f0aae647137a8da063c64831bd833
Rebeca Suárez <rsbascon1998@gmail.com>
2020-04-21 13:37:42 +0200

    Formulario de inicio de la librería

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+    
+        <form action="" method="post">
+            <h2>Librería La Campiña</h2>
+            <h3>BIENVENID@ A NUESTRA PÁGINA WEB</h3>
+            <hr>
+            <p>Si eres nuevo te invitamos a unirte a 
+            <br />nosotros. Si ya formas parte de nuestra <br />
+            comunidad, adelante.</p>
+            <input type="submit" name="registro" value="Registro" />
+            <input type="submit" name="inicio_sesion" value="Inicio Sesión" />
+        </form>
+
+        <?php
+            
+            include 'conexionBD.php';
+            mysqli_select_db($conexion, $nombreBD);
+            
+            if (isset($_POST['registro'])) {     
+                header('Location:./registro.php');
+            }
+
+            elseif (isset($_POST['inicio_sesion'])) {
+                header('Location:./inicio_sesion.php');
+            }
+
+            ?>
+
+    </body>
+</html>
