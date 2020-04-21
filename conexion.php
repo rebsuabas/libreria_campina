e7cff806399904ea903e45dece7d5d59553f4c42
Rebeca Suárez <rsbascon1998@gmail.com>
2020-04-21 18:22:54 +0200

    Conexion BD con PDO

--- conexion.php
+++ conexion.php
@@ -1,14 +1,8 @@
 <?php
 
-    $user="root";
-    $pwd="2asir";
-    
-    try {
-        $conexion = new PDO('mysql:host=localhost;dbname=libreria_campina', $user, $pwd);
-    }
-    catch(PDOException $e) {
-        echo $e->getMessage();
-    }
+    $user='root';
+    $pwd='2asir';
+    $conexion = new PDO('mysql:host=localhost;dbname=libreria_campina', $user, $pwd);
     
 
 ?>
