465ac6df1ce06abf4caeb9a07ce4d941ae9346c7
Rebeca Suárez <rsbascon1998@gmail.com>
2020-04-22 19:51:39 +0200

    Confirmación de registro

---
+++ confirmacion_registro.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <h2>Librería La Campiña</h2>
+        <h3>BIENVENID@ A NUESTRA PÁGINA WEB</h3>
+        <hr>
+        <br />
+        <p>Ya has sido registrad@, ¡Bienvenid@ a nuestra comunidad!</p>
+        <p><b>¿Qué deseas hacer a continuación?</b></p>
+        <br />
+        <input type="submit" name="inicio_sesion" value="Inicio sesión">
+        <input type="submit" name="salir" value="Salir">
+    </body>
+</html>
