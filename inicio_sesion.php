fd64976b549bb5501bc4e92e23658771ecdeb6e0
Rebeca Suárez <rsbascon1998@gmail.com>
2020-04-25 12:12:00 +0200

    Formulario de inicio sesión

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -1,1 +1,33 @@
 <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <h2>Inicio Sesión</h2>
+            <hr>
+            <br />
+            <label><b>Usuario</b></label> 
+            <br />
+            <br />
+            <input type="text" name="usuario" required>
+            <br />
+            <br />
+            <br />
+            <label><b>Contraseña</b></label>
+            <br />
+            <br />
+            <input type="password" name="contrasena" required>
+            <br />
+            <br />
+            <input type="checkbox" name="recordar">
+            <label>Recordar<label>
+            <br />
+            <br />
+            <br />
+            <input type="submit" name="iniciar_sesion" value="Iniciar Sesión">
+            <input type="submit" name="salir" value="Salir">
+        </form>
+    </body>
+</html>
