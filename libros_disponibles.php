c9f4d6d058582d9e31dc74d86997f1118ce77669
Rebeca Suárez <rsbascon1998@gmail.com>
2020-04-27 20:24:51 +0200

    Catalogos y libros disponibles

---
+++ libros_disponibles.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="StyleSheet" href="./css/estilo_libros_autores.css" type="text/css" />
+    </head>
+    <body>
+        <div id="contenedor">
+            <form method="POST" action="">
+                <div id="cabecera">
+                    <h2>Librería La Campiña</h2>
+                </div>
+                <hr>
+                <div id="contenido">
+                    <div id="cerrar_sesion">
+                        <input type="submit" name="cerrar_sesion" value="Cerrar Sesión">
+                    </div>
+                    <div id="titulo">
+                        <h3>LIBROS DISPONIBLES</h3>
+                    </div>
+                    <div id="lista_libros">
+
+                    </div>
+                    <div id="volver">
+                        <input type="submit" name="volver" value="Volver">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
