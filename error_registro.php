806181c77f7d77fac2525d3c0cb92129be22cd0d
Rebeca Suárez <rsbascon1998@gmail.com>
2020-05-12 15:25:46 +0200

    Formularios de error + css

---
+++ error_registro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="StyleSheet" href="./css/estilo_error.css" type="text/css" />
+    </head>
+    <body>
+        <div id="contenedor">
+            <form method="POST" action="">
+                <div id="cabecera">
+                    <h2>Librería La Campiña</h2>
+                </div>
+                <hr>
+                <div id="texto">
+                    <p>El usuario no ha podido ser registrado.</p>
+                </div>
+                <div id="botones_reg">
+                    <input type="submit" name="volver" value="Volver"class="botones">
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
