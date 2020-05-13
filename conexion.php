4ad7183a308fe3b2981e5c2495ad06f7a38b62be
Rebeca Suárez <rsbascon1998@gmail.com>
2020-05-13 15:37:40 +0200

    Corrección en la conexion de BD

--- conexion.php
+++ conexion.php
@@ -1,7 +1,32 @@
 <?php
 
-    $user='root';
-    $pwd='2asir';
-    $conexion = new PDO('mysql:host=localhost;dbname=libreria_campina', $user, $pwd);
+    class Conexion { 
+        
+        public $host = "localhost";
+        public $dbname = "libreria_campina";
+        public $user = "root";
+        public $pwd = "2asir"; 
+        
+        public function Conectar() {
+           
+            try {
+                $connection = "mysql:host=$this->host;dbname=$this->dbname";
+                $user = $this->user;
+                $pwd = $this->pwd;
+
+                $options = array (
+                    PDO::MYSQL_ATTR_LOCAL_INFILE => 1,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+                
+                $pdo = new PDO ($connection, $user, $pwd, $options);
+                return $pdo;
+            }catch(PDOException $e){      
+                print_r("Error connection: " . $e->getMessage());
+            }
+        }
+        
+        
+    }
     
 ?>
