2ec4cb7164ce70ab118223711c068320292ff52c
Rebeca Suárez <rsbascon1998@gmail.com>
2020-05-15 20:22:49 +0200

    Cambiar de carpeta

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+
+    /*include_once './clases/Usuario.php';
+    include_once './clases/sesion.php';
+
+    $sesionUsuario = new Sesion();
+    $usuario = new Usuario();
+
+    if (isset($_SESSION['usuario'])) {
+        echo "Hay sesión";
+    } elseif (isset($_POST['id_usuario']) && isset($_POST['contrasena'])) {
+        echo "Validación de login";
+
+        $userForm=$_POST['id_usuario'];
+        $passForm=$_POST['contrasena'];
+
+        if ($usuario->existeUsuario($userForm, $passForm)) {
+            echo "Usuario validado.";
+        } else {
+            echo "Error en la validacion";
+        }
+        
+
+    } else {
+        echo "Login";
+        header('Location:./inicio_sesion.php');
+    }*/
+    
+
+?>
